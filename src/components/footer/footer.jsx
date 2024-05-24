498fc9b1eca410b4501c0e8930db6aabe4a2ec1f
gameliaprogrammer <eng.gameliaahmed@gmail.com>
2024-05-24 22:18:41 +0300

    THis first step

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,15 @@
+import   './footer.css';
+
+
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+    Designed and developed by
+    <span> Jamila tranning for Next.js</span>© 2024.
+  </footer>
+  );
+}
+
+export default Footer;
